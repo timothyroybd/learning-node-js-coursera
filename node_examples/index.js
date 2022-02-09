84fc63f4c873c3e94ed9ae2fdc0eacb7dc8afe11
Timothy <timothyroy94@protonmail.com>
2022-02-09 15:40:00 +0600

    first lesson

---
+++ node_examples/index.js
@@ -0,0 +1,24 @@
+const rect = {
+  perimeter: (x, y) => 2 * (x + y),
+  area: (x, y) => x * y,
+};
+
+function solveRect(l, b) {
+  console.log("Solving for rect with l = " + l + "and b =" + b);
+  if (l <= 0 || b <= 0) {
+    console.log(
+      "Rectangle dimensions shoudl be greater than zero: l =" +
+        l +
+        ", and b =" +
+        b
+    );
+  } else {
+    console.log(`The area of the rectangle is ${rect.area(l, b)}`);
+    console.log(`The peremiter of the rectangle is ${rect.perimeter(l, b)}`);
+  }
+}
+
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(-3, 5);
