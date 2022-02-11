c2e85dc559555e329a6000110d22edab6bc35658
Timothy <timothyroy94@protonmail.com>
2022-02-11 11:47:36 +0600

    express router

--- rest_api/index.js
+++ rest_api/index.js
@@ -3,6 +3,7 @@ const http = require("http");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
+const dishRouter = require("./routes/dishRouter");
 const hostName = "localhost";
 const port = 3000;
 
@@ -11,43 +12,8 @@ app.use(morgan("dev"));
 //use this middleware to serve static HTML files
 app.use(bodyParser.json());
 //Hey
-app.all("/dishes", (req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  next();
-});
-app.get("/dishes", (req, res) => {
-  res.end("Will send all the dishes to you!");
-});
-app.post("/dishes", (req, res) => {
-  res.end(
-    `Will add the dish: ${req.body.name} with details: ${req.body.description}`
-  );
-});
-app.put("/dishes", (req, res) => {
-  res.statusCode = 403;
-  res.end(`Put operation not supported on /dishes`);
-});
-app.delete("/dishes", (req, res) => {
-  res.end(`Deleting all the dishes`);
-});
 
-app.get("/dishes/:dishId", (req, res) => {
-  res.end(`We will send details of the dish: ${req.params.dishId} to you`);
-});
-app.post("/dishes/:dishId", (req, res) => {
-  res.statusCode = 403;
-  res.end(`POST operation not supported on /dishes/${req.params.dishId}`);
-});
-app.put("/dishes/:dishId", (req, res) => {
-  res.write(`Updating the dish: ${req.params.dishId}`);
-  res.end(
-    `Will update the dish: ${req.params.dishId}  with details ${req.body.description}`
-  );
-});
-app.delete("/dishes/:dishId", (req, res) => {
-  res.end(`Deleting  dishe ${req.params.dishId}`);
-});
+app.use("/dishes", dishRouter);
 app.use(express.static(__dirname + "/public"));
 app.use((req, res, next) => {
   res.statusCode = 200;
@@ -59,6 +25,3 @@ const server = http.createServer(app);
 server.listen(port, hostName, () => {
   console.log(`listening to port no ${hostName}:${port}`);
 });
-function nothing() {
-  console.log("I am doing nothing");
-}
